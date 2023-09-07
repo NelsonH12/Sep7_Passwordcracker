a89c038583e6676b215e373a0b86a55b11f16c90
Nelson Hopp <108594613+NelsonH12@users.noreply.github.com>
2023-09-07 12:11:56 -0400

    Add files via upload

---
+++ pwcracker.py
@@ -0,0 +1,21 @@
+import random
+import pyautogui
+import time
+
+chars = "abcdefghijklmnopqrstuvwxyz123456789"
+
+allchar = list(chars)
+
+pwd = pyautogui.password("Enter a password:")
+
+sample_pwd = ""
+
+while(sample_pwd != pwd):
+  sample_pwd = random.choices(allchar, k=len(pwd))
+
+  print("<====" + str(sample_pwd) + "====>")
+
+  if sample_pwd == list(pwd):
+    print("The password is: " + "".join(sample_pwd))
+    time.sleep(3)
+    break
